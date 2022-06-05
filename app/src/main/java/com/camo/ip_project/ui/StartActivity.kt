18727e0b025dde870a801e4bfe7b597e4f47b4d6
camo0112 <56369484+camo0112@users.noreply.github.com>
2022-06-05 12:44:44 +0530

    Updated Copyright

--- app/src/main/java/com/camo/ip_project/ui/StartActivity.kt
+++ app/src/main/java/com/camo/ip_project/ui/StartActivity.kt
@@ -1,6 +1,6 @@
 /*****************************************************************************************
  * Copyright <2022> <Saurav Rao> <sauravrao637@gmail.com>                                *
- *                                                                                       *
+ * Copyright <2022> <Piyush Sharma> <piyushlp@gmail.com>                                 *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
  * software and associated documentation files (the "Software"), to deal in the Software *
  * without restriction, including without limitation the rights to use, copy, modify,    *
--- app/src/main/java/com/camo/ip_project/ui/Utility.kt
+++ app/src/main/java/com/camo/ip_project/ui/Utility.kt
@@ -1,6 +1,6 @@
-/****************************************************************************************
+/*****************************************************************************************
  * Copyright <2022> <Saurav Rao> <sauravrao637@gmail.com>                                *
- *                                                                                       *
+ * Copyright <2022> <Piyush Sharma> <piyushlp@gmail.com>                                 *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
  * software and associated documentation files (the "Software"), to deal in the Software *
  * without restriction, including without limitation the rights to use, copy, modify,    *
@@ -9,8 +9,7 @@
  * conditions:                                                                           *
  *                                                                                       *
  * The above copyright notice and this permission notice shall be included in all copies *
- * or substantial portions of the Software.
- *
+ * or substantial portions of the Software.                                              *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
  * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
  * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
